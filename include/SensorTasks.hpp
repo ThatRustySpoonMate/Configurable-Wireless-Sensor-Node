eeef05b36d8892dc76f7558aa289380d8657adcd
Ethan Harris <ethanhharris5@gmail.com>
2024-08-10 13:20:41 +1000

    Improved sensor definitions

--- include/SensorTasks.hpp
+++ include/SensorTasks.hpp
@@ -6,10 +6,26 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+/* Define connected devices here */
+#define SOIL_MOISTURE_SENSOR
+#define BME280
+
+
+/* Configure connected devices here */
+#ifdef SOIL_MOISTURE_SENSOR
 #define SOIL_MOISUTRE_SENS_VCC 7
 #define SOIL_MOISTURE_SENS_DIN 8 // 5 - ADC1_CH4 for ESP32 Dev Board
+#define SOIL_MOISTURE_SETTLE_TIME 250
+uint32_t init_soil_sensor();
+#endif
+
+#ifdef BME280
+uint32_t init_bme280();
+#endif
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+
+
 
 void initSensors();
 
--- src/SensorTasks.cpp
+++ src/SensorTasks.cpp
@@ -1,44 +1,68 @@
 #include "SensorTasks.hpp"
 
+
+#ifdef BME280
 Adafruit_BME280 bme; // I2C
+#endif
 
 void initSensors() {
-    unsigned status;
+  
+  #ifdef SOIL_MOISTURE_SENSOR
+  init_soil_sensor();
+  #endif
 
-    /* Initialize soil moisture sensors */ 
-    pinMode(SOIL_MOISUTRE_SENS_VCC, PULLDOWN);
-
-    /* Initialize BME280 */
-    status = bme.begin();  
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-    }
-
-    //bme.init();
+  #ifdef BME280
+  init_bme280();
+  #endif
 
 
-    return;
+  return;
 }
 
 void readSensors(uint16_t *moistureReading, float *temp, float *humidity, float *baroPres, float *altitude) {
+
   // Power up Soil sensor
+  #ifdef SOIL_MOISTURE_SENSOR
   digitalWrite(SOIL_MOISUTRE_SENS_VCC, HIGH);
+  #endif
 
   /* Read values from BME280 */
+  #ifdef BME280
   *temp = bme.readTemperature();
   *humidity = bme.readHumidity();
   *baroPres = bme.readPressure();
   *altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  #endif
 
-  // Allow time for startup of soil moisture sensor
-  delay(500); 
-  // Take Reading from Soil moisture sensor
+  // Allow time for startup  of soil moisture sensor before taking reading
+  #ifdef SOIL_MOISTURE_SENSOR
+  delay(SOIL_MOISTURE_SETTLE_TIME); 
   *moistureReading = analogRead(SOIL_MOISTURE_SENS_DIN);
-
-    // Power down soil sensor
-  digitalWrite(SOIL_MOISUTRE_SENS_VCC, LOW);
+  digitalWrite(SOIL_MOISUTRE_SENS_VCC, LOW);  // Power down soil sensor
+  #endif
   
-
   return;
+}
+
+uint32_t init_soil_sensor() {
+  /* Initialize soil moisture sensors */ 
+  pinMode(SOIL_MOISUTRE_SENS_VCC, PULLDOWN);
+
+  return 0;
+}
+
+
+
+uint32_t init_bme280() {
+  uint32_t status;
+  /* Initialize BME280 */
+  status = bme.begin();  
+  if (!status) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+      Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+  }
+
+  //bme.init();
+
+  return status;
 }
