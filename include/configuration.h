a550e3ca7167965de2a6af59fddc2a936ae0314f
Ethan Harris <51020883+ThatRustySpoonMate@users.noreply.github.com>
2025-10-03 23:52:34 +1000

    Moved ens160 configuration

--- include/configuration.h
+++ include/configuration.h
@@ -110,6 +110,10 @@
 // ENS160
 #ifdef DEVICE_ENS160
 #define ENS160_I2C_ADDRESS 0x53     // 0X52: ADDR low, 0x53: ADDR high
+#define ENS160_IDLE_AFTER_READING false // If true, will enter idle state after a reading is taken, this presumably lowers power consumption, but results in a longer startup time for next reading. Only set to true if you have a large value for DEFAULT_SLEEP_TIME_SECONDS (e.g. 10+ minutes)
+#define ENS160_STATE_FIRST_STARTUP_POLL_INTERVAL 25000
+#define ENS160_STATE_WARMING_POLL_INTERVAL 3000
+#define ENS160_STATE_INVALID_POLL_INTERVAL 50
 #endif
 
 // DHT11 Sensor Configuration
@@ -146,10 +150,6 @@
 #define ENS160_AQI_ID 0
 #define ENS160_TVOC_ID 0
 #define ENS160_ECO2_ID 0
-#define ENS160_IDLE_AFTER_READING false // If true, will enter idle state after a reading is taken, this presumably lowers power consumption, but results in a longer startup time for next reading. Only set to true if you have a large value for DEFAULT_SLEEP_TIME_SECONDS (e.g. 10+ minutes)
-#define ENS160_STATE_FIRST_STARTUP_POLL_INTERVAL 25000
-#define ENS160_STATE_WARMING_POLL_INTERVAL 3000
-#define ENS160_STATE_INVALID_POLL_INTERVAL 50
 #endif
 
 #if defined(DEVICE_CAPACITIVE_SOIL_MOISTURE_SENSOR)
